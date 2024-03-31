d448e000458723980c98779cc9ab569f32b5df75
siigil <git@kknowl.es>
2024-03-31 12:08:42 -0400

    Bug fixes & updates
    Updated functionality to work as intended and smoothed several features:
    - Removed test output lines
    - Consistent tool output (e.g. [-])
    - Skip reporting 404 for "Users" role as it's never found
    - Combine methods for user, group, SP (list_users, list_sps, list_groups, etc) into single functions for convenience w/ global variables for endpoint types
    - Usage examples added to README
    - Quit early if no term matches are found
    - Fixed reverse role-map function (-r)
    - Set default output name

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # azure-tools
 Personal tools for working in Azure. Nothing too fancy, just a collection of what's handy.
 
-### aad-role-audit.py
-Based on an az CLI session, determine if a user, group, or Service Principal has Entra ID / Azure AD role assignments (Directory or Custom), and deliver a final output with details. Does not yet include PIM.
+## aad-role-audit.py
+Based on an az CLI session, determine if a User, Group, or Service Principal has current Entra ID / Azure AD role assignments (Directory or Custom), and deliver a final output with details:
+- Return results based on Group, User, SP, or Role
+- Options for name (-n) and id (-i) matching
+- Current results returned in JSON (-o) and CLI findings
+### Examples:
+#### Return role assignments for groups starting with term "Test"
+python3 aad-role-audit.py -gn "Test"
+#### Return role assignments for "Global" roles ("Global Administrator", "Global Reader")
+python3 aad-role-audit.py -rn "Global"
+#### Return role assignments for users starting with "Z" in a file "2024-03-users.json" (default is output-users.json)
+python3 aad-role-audit.py -un Z -o 2024-03
+#### Return role assignments for "Global Reader" by role ID (can be used with users, groups, & SPs as well)
+python3 aad-role-audit.py -ri f2ef992c-3afb-46b9-b7cf-a126ee74c451
 
-### az-membership-enum.py
+## az-membership-enum.py
 For a given CSV list of Azure users (exported from Azure portal or otherwise) collect AAD membership details using az ad CLI tools.
 
-### graph-test-template.py
+## graph-test-template.py
 Convert an Azure CLI token to Graph, and make a query to a test MS Graph endpoint. Used as a base for quick prototyping and testing of Graph API endpoints.
--- aad-role-audit.py
+++ aad-role-audit.py
@@ -12,6 +12,13 @@ import json
 import subprocess
 import argparse
 
+typename_sp = "SPs"
+endpoint_sp = "servicePrincipals"
+typename_user = "Users"
+endpoint_user = "users"
+typename_group = "Groups"
+endpoint_group = "groups"
+
 class colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -38,7 +45,7 @@ def progress_bar(length,progress,psym="#",usym="-"):
     progress = (progress) / length
     bar = psym * int(bar_length * progress)
     bar = bar.ljust(bar_length, usym)
-    print(f"\r{colors.OKBLUE}[{bar}]{colors.ENDC} {int(progress * 100)}%", end='')
+    print(f"\r[-] {colors.OKBLUE}[{bar}]{colors.ENDC} {int(progress * 100)}%", end='')
 
 # Make Graph API calls
 def call(token, url):
@@ -51,69 +58,47 @@ def call(token, url):
     return reply
 
 # Collect Users
-def list_users(token,oid='',name=''):
+# using method in 74 & 75, return an error flag and then break later on.
+def list_members(token,typename,endpoint,oid='',name=''):
+    matches = True
     if oid:
-        print(f"{colors.ITAL}Collecting User with id \'{oid}\'.{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/users?$filter=id eq '{oid}'"
+        print(f"[-] Collecting {typename} with id \'{oid}\'.")
+        url = f"https://graph.microsoft.com/v1.0/{endpoint}?$filter=id eq '{oid}'"
     elif name:
-        print(f"{colors.ITAL}Collecting Users with name \'{name}\'.{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/users?$filter=displayName eq '{name}'"
+        print(f"[-] Collecting {typename} matching & starting with name term \'{name}\'.")
+        url = f"https://graph.microsoft.com/v1.0/{endpoint}?$filter=startsWith(displayName,'{name}')"
     else:
-        print(f"{colors.ITAL}Collecting all Users...{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/users"
-    users = call(token,url)
-    print(users)
-    return users
-
-# Collect Groups
-def list_groups(token,oid='',name=''):
-    if oid:
-        print(f"{colors.ITAL}Collecting Group with id \'{oid}\'.{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/groups?$filter=id eq '{oid}'"
-    elif name:
-        print(f"{colors.ITAL}Collecting Groups with name \'{name}\'.{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/groups?$filter=displayName eq '{name}'"
-    else:
-        print(f"{colors.ITAL}Collecting all Groups...{colors.ENDC}")
-        url = f"https://graph.microsoft.com/v1.0/groups"
-    groups = call(token,url)
-    return groups
-
-# Collect Service Principals
-def list_sps(token,oid='',name=''):
-    if oid:
-        print(f"{colors.ITAL}Collecting SP with id \'{oid}\'.{colors.ITAL}")
-        url = f"https://graph.microsoft.com/v1.0/servicePrincipals?$filter=id eq '{oid}'"
-    elif name:
-        print(f"{colors.ITAL}Collecting SPs with name \'{name}\'.{colors.ITAL}")
-        url = f"https://graph.microsoft.com/v1.0/servicePrincipals?$filter=displayName eq '{name}'"
-    else:
-        print(f"{colors.ITAL}Collecting all SPs...{colors.ITAL}")
-        url = f"https://graph.microsoft.com/v1.0/servicePrincipals"
-    sps = call(token,url)
-    return sps
+        print(f"[-] Collecting all {typename}...")
+        url = f"https://graph.microsoft.com/v1.0/{endpoint}"
+    items = call(token,url)
+    if not items.get('value',[]):
+        print("[!] No matches found.")
+        matches = False
+    print(f"[-] {typename} found.")
+    return items, matches
 
 # Collect Roles
 def list_roles(token,oid='',name=''):
     # Role Definition endpoint does not support filter, so need to filter client-side
-    print("\x1B[3mCollecting Role(s)...\x1B[0m", end =' ')
+    print("[-] Collecting Role(s)...")
     url = f"https://graph.microsoft.com/v1.0/roleManagement/directory/roleDefinitions?$select=displayName,id"
     roles = call(token,url)
     if oid:
-        print(f"\x1B[3mRole ID: \'{oid}\'.\x1B[0m")
+        print(f"[+] Role ID: \'{oid}\'.")
         # Filter to just the role we're looking for
         i_role = [role for role in roles['value'] if role['id'] == oid]
         # Nest back into expected dictionary format
         roles = {'value': i_role}
     elif name:
-        print(f"\x1B[3mRole Name: \'{name}\'.\x1B[0m")
-        n_role = [role for role in roles['value'] if role['displayName'] == name]
+        print(f"[+] Role Name Term: \'{name}\'.")
+        n_role = [role for role in roles['value'] if role['displayName'].startswith(name)]
         roles = {'value': n_role}
+    print("[-] Roles found.")
     return roles
 
 # For Groups, enhance with Owners & *Groups* the Group is a MemberOf (this will be Group or sometimes Role details)
 def list_group_owners_memberof(token,groups):
-    print("Fetching group details...")
+    print(f"[-] Fetching group details...")
     all_groups = groups.get('value',[])
     length = len(all_groups)
 
@@ -144,12 +129,12 @@ def list_group_owners_memberof(token,groups):
         group.update(groupMemberOfName)
         group.update(groupMemberOfId)
     
-    print("\nFinished getting Group details.")
+    print("\n[-] Finished getting Group details.")
     return groups
 
 # For Groups, enhance with members
 def list_group_members(token,groups):
-    print("Collecting Group members...")
+    print("[-] Collecting Group members...")
     # Prepare URL & query membership for each group
     for index, group in enumerate(groups['value']):
         id = group['id']
@@ -170,7 +155,7 @@ def get_role_members(roles, token):
     headers = {
         'Authorization': f'Bearer {token}'
     }
-    print("Getting group membership of all roles...")
+    print(f"[-] Getting group membership of all roles...")
     all_roles = roles.get('value',[])
     length = len(all_roles)
 
@@ -185,45 +170,47 @@ def get_role_members(roles, token):
         try:
             roles_members.raise_for_status()
         except requests.exceptions.HTTPError as err:
-            print(f"\nRole ID for '{role['displayName']}' ({role['id']}) not found ({err}). Skipping.")
+            if role['id'] == 'a0b1b346-4d3e-4e8b-98f8-753987be4970':
+                pass
+            else:
+                print(f"\n[!] Role ID for '{role['displayName']}' ({role['id']}) not found ({err}). Skipping.")
             ids = []
         else:
             role_members = roles_members.json()
             # Pull out list of member-groups for the role
             ids = [item['principalId'] for item in role_members['value']]
-            # Update the role item wiht the list of member-groups
+            # Update the role item with the list of member-groups
         members = {"members": ids}
         role.update(members)
     return roles
 
 # For Roles & Groups/Users, find matches
 def find_member_roles(roles, objects, cat=''):
-    print(f"\nFinding {cat} role matches...")
+    print(f"\n[-] Finding {cat} role matches...")
 
     for obj in objects['value']:
-        # Initialize group entries to store role match info
+    # Initialize group entries to store role match info
         obj.update({"roleNames":[]})
         obj.update({"roleIds":[]})
-    
+
     # Set up roles for enumeration
     all_roles = roles.get('value',[])
     matches = False
 
     # For each role, check if any members match with our groups of interest. If so, add the corresponding role back to the group.
     for index, role in enumerate(all_roles):
-        #progress_bar(length,index+1)
         for member in role['members']:
             for obj in objects['value']:
                 if member.strip() == obj['id'].strip():
                     # Print matches out so we know what's been found
-                    print(f"Role: {role['displayName']} ({role['id']}), Member: {obj['displayName']} ({obj['id']}).")
+                    print(f"[+] Role: {role['displayName']} ({role['id']}), Member: {obj['displayName']} ({obj['id']}).")
                     obj['roleNames'].append(role['displayName'])
                     obj['roleIds'].append(role['id'])
                     matches = True
     if matches == True:
-        print(f"{cat} role assignments were found.")
+        print(f"[+] {cat} role assignments were found.")
     elif matches == False:
-        print(f"No {cat} role assignments found.")
+        print(f"[!] No {cat} role assignments found.")
     return objects
 
 # Prepare a CSV report of JSON data based on the provided Groups object & its members
@@ -239,46 +226,44 @@ def group_csv_report(groups, filename):
                 report.writerow([group['displayName'],member,upn,name])
     csvfile.close()
 
-def handle_user_roles(token, output, oid='', name=''):
-    print("Beginning user role enumeration.")
-    users=list_users(token,oid,name)
+# Testing using for everything
+def handle_item_roles(token, output, typename, endpoint, oid='', name=''):
+    print(f"[-] Beginning {endpoint} role enumeration.")
+    items,matches=list_members(token,typename,endpoint,oid,name)
+    if not matches:
+        print("[!] Quitting.")
+        return
     roles=list_roles(token)
     roles=get_role_members(roles,token)
-    users=find_member_roles(roles,users,"User")
-    with open(f"{output}-users.json",'w') as f:
-        json.dump(users,f,indent=4)
-
-def handle_sp_roles(token, output, oid='', name=''):
-    print("Beginning SP role enumeration.")
-    sps=list_sps(token,oid,name)
-    roles=list_roles(token)
-    roles=get_role_members(roles,token)
-    users=find_member_roles(roles,sps,"SP")
-    with open(f"{output}-sps.json",'w') as f:
-        json.dump(users,f,indent=4)
-
-def handle_group_roles(token, output, oid='', name=''):
-    print("Beginning group role enumeration.")
-    groups=list_groups(token,oid,name)
-    groups=list_group_owners_memberof(token,groups)
-    roles=list_roles(token)
-    roles=get_role_members(roles,token)
-    groups=find_member_roles(roles,groups,"Group")
-    with open(f"{output}-groups.json",'w') as f:
-        json.dump(groups,f,indent=4)
+    items=find_member_roles(roles,items,typename)
+    with open(f"{output}-{endpoint}.json",'w') as f:
+        json.dump(items,f,indent=4)
 
 def handle_roles(token, output, oid='', name=''):
-    print("Beginning role member enumeration.")
+    print("[-] Beginning role member enumeration.")
     # Gather users, groups, and target role
-    users=list_users(token)
-    groups=list_groups(token)
+    users,matches=list_members(token,typename_user,endpoint_user)
+    groups,matches=list_members(token,typename_group,endpoint_group)
+    sps,matches=list_members(token,typename_sp,endpoint_sp)
+    # Get roles, matching key terms if needed
     role=list_roles(token,oid,name)
     role=get_role_members(role,token)
-    users=find_member_roles(role,users,"User")
-    groups=find_member_roles(role,groups,"Group")
-    
-    with open(f"{output}-roles.json",'w') as f:
+    # Enrich members with role where applicable
+    users=find_member_roles(role,users,typename_user)
+    groups=find_member_roles(role,groups,typename_group)
+    sps=find_member_roles(role,sps,typename_sp)
+    if oid:
+        roleschecked = oid
+    if name:
+        roleschecked = name
+    else:
+        roleschecked = "roles"
+    with open(f"{output}-user-{roleschecked}.json",'w') as f:
         json.dump(users,f,indent=4)
+    with open(f"{output}-group-{roleschecked}.json",'w') as f:
+        json.dump(groups,f,indent=4)
+    with open(f"{output}-sp-{roleschecked}.json",'w') as f:
+        json.dump(sps,f,indent=4)
 
 def main():
     # Parse arguments
@@ -298,34 +283,28 @@ def main():
 
     # Verify search and name are not both set
 
+    if not args.output:
+        args.output = "output"
+
     if args.users == True:
-        handle_user_roles(token,args.output,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_user,endpoint_user,args.oid,args.name)
 
     elif args.groups == True:
-        handle_group_roles(token,args.output,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_group,endpoint_group,args.oid,args.name)
 
     elif args.sp == True:
-        handle_sp_roles(token,args.output,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_sp,endpoint_sp,args.oid,args.name)
 
     elif args.all == True:
-        handle_user_roles(token,args.output,args.oid,args.name)
-        handle_group_roles(token,args.output,args.oid,args.name)
-        handle_sp_roles(token,args.output,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_user,endpoint_user,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_group,endpoint_group,args.oid,args.name)
+        handle_item_roles(token,args.output,typename_sp,endpoint_sp,args.oid,args.name)
         
     elif args.role == True:
         handle_roles(token,args.output,args.oid,args.name)
 
     else:
-        print("Please specify --users or --groups, or -h for help.")
+        print("[!] Please specify --users or --groups, or -h for help.")
 
 if __name__ == "__main__":
     main()
-
-############################
-# Future Possible Features: 
-# - Add PIM functionality - Can't check for PIM roles without additional assignmetns in the Graph API (api.azrbac.mspim.azure.com is not available to users)
-# - Save the Roles as a DB & import
-# - Smooth reporting functionality - default = CLI, otherwise JSON dump or report - set whether to list all matches or not
-# - Get fields for dynamic groups and put into final report
-# - Prettier output
-# - [Later] Token loading methods, vs just 'az auth'.
